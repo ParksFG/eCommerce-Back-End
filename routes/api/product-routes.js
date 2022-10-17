9b8bbd624b7e72ce50ef10a5bc83f6eb1c918636
ParksFG <ParkerFGay@gmail.com>
2022-10-17 05:09:29 -0500

    GET route for single product

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -19,9 +19,19 @@ router.get('/', async (req, res) => {
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const singleProductData = await Product.findByPk(req.params.id, {
+      include: [Category,{
+        model: Tag,
+        through: ProductTag
+      }]
+    })
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 // create new product
