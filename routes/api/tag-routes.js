0d5ee3de53fc8f8f86275c843b8ed3514d26b723
ParksFG <ParkerFGay@gmail.com>
2022-10-17 05:51:28 -0500

    GET tag by ID

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -16,9 +16,17 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
-  // find a single tag by its `id`
-  // be sure to include its associated Product data
+router.get('/:id', async (req, res) => {
+  try {
+    const singleTagData = await Tag.findByPk(req.params.id, {
+      include: [Product,{
+        model: Product,
+        through: ProductTag
+      }]
+    })
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
