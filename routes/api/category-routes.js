537e70f172218f7b9b9deeb9ba1e27ce6a98f640
ParksFG <ParkerFGay@gmail.com>
2022-10-17 06:07:40 -0500

    DELETE delete category by ID route

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -55,8 +55,18 @@ router.put('/:id', async (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const deleteCategory =  await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    res.json(deleteCategory)
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 module.exports = router;
